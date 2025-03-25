169f62836f6a4b60d34c4309ffdc119be3361670
bettytadege <bettytadege13@gmail.com>
2025-03-25 10:55:55 +0300

    refactor:add swaggar and validation pipe

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,23 @@
 
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
+
+const PORT = process.env.PORT;
+console.log(PORT);
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(AppModule, { cors: true });
+  const config = new DocumentBuilder()
+    .setTitle('eccomerce')
+    .setDescription('eccomerce api ')
+    .addTag('eccomerce')
+    .setVersion('1.0')
+    .build();
+    const documentFactory=SwaggerModule.createDocument(app,config)
+    SwaggerModule.setup('api',app,documentFactory)
+    app.useGlobalPipes(new ValidationPipe());
+  await app.listen(PORT ?? 3000);
 }
- bootstrap();
+bootstrap();
