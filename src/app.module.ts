ac745da9921e3efa34fa592381557e576a0adec2
bettytadege <bettytadege13@gmail.com>
2025-03-25 10:59:37 +0300

    refactor:add config module,catagory module,productmodule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { CatagoryModule } from './catagory/catagory.module';
+import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [AuthModule],
+  // imports: [AuthModule],
+  imports: [
+    AuthModule,
+    ConfigModule.forRoot({
+      isGlobal: true, 
+      envFilePath:'.env'
+    }),
+    CatagoryModule,
+    ProductModule,
+  ],
+
   controllers: [AppController],
   providers: [AppService],
 })
