845d6aae59161b406eee69e0ee4d49d2e0d3179e
bettytadge <bettytadge132gmail.com>
2025-04-12 22:19:00 +0300

    refactor:add new post request

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -5,9 +5,19 @@ import { AuthService } from './auth.service';
 @Controller('auth')
 export class AuthController {
     constructor(private readonly authService :AuthService){}
-@Post('signin')
-    signIn(@Body('idToken') idToken:string){
+@Post('google-signin')
+googleSignIn(@Body('idToken') idToken:string){
         console.log('recieved idtoken',idToken)
+        return this.authService.googleSignIn(idToken)
+    }
+    
+    @Post('signin')
+    signIn(@Body('idToken') idToken:string){
         return this.authService.signIn(idToken)
     }
+    @Post('signup')
+    signUp(@Body('idToken') idToken:string){
+        return this.authService.signUp(idToken)
+    }
+
 }
