a61036fa35ad72158233c0a12a5c7862cfb68cd3
bettytadge <bettytadge132gmail.com>
2025-04-13 17:32:00 +0300

    refactor:update controller name

--- src/catagory/catagory.controller.ts
+++ src/catagory/catagory.controller.ts
@@ -1,31 +1,34 @@
 /* eslint-disable prettier/prettier */
-import { CreateCatagoryDto } from './dto/create.catagory.dto';
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+
 import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 import { CatagoryService } from './catagory.service';
-import { UpdateCatagoryDto } from './dto/update.catagory.dto';
+import { CreateMainCategoryDto } from './dto/create.catagory.dto';
+import { UpdateMainCatagoryDto } from './dto/update.catagory.dto';
+// import { UpdateCatagoryDto } from './dto/update.catagory.dto';
 
 @Controller('catagories')
 export class CatagoryController {
     constructor( private readonly catagoryService:CatagoryService){}
     @Post('create')
-    async create(@Body() createCatagoryDto:CreateCatagoryDto){
-     return this.catagoryService.create(createCatagoryDto)
+    async create(@Body() createMainCatagoryDto: CreateMainCategoryDto){
+     return this.catagoryService.create(createMainCatagoryDto)
     }
     @Get('/')
     async findAll(){
         return this.catagoryService.findAll()
     }
     @Get(':id')
-    async findById(@Param('id') id:string){
-        return this.catagoryService.findById(id)
+    async findOne(@Param('id') id:string){
+        return this.catagoryService.findOne(id)
     }
     @Put(':id')
-    async Update(@Param('id') id:string ,@Body() updateCatagoryDto:UpdateCatagoryDto){
-        return this.catagoryService.update(id,updateCatagoryDto)
+    async Update(@Param('id') id:string ,@Body() updateMainCatagoryDto:UpdateMainCatagoryDto){
+        return this.catagoryService.update(id,updateMainCatagoryDto)
     }
     @Delete(':id')
-    async delete(@Param('id') id:string){
-        return this.catagoryService.delete(id)
+    async remove(@Param('id') id:string){
+        return this.catagoryService.remove(id)
 
     }
 }
