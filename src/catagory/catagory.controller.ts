500b4a8eae809c102539f49e4f223332c5c63468
bettytadege <bettytadege13@gmail.com>
2025-04-21 18:20:13 +0300

    fix: fix spelling error

--- src/catagory/catagory.controller.ts
+++ src/catagory/catagory.controller.ts
@@ -1,34 +1,49 @@
 /* eslint-disable prettier/prettier */
 /* eslint-disable @typescript-eslint/no-unsafe-return */
 
-import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
-import { CatagoryService } from './catagory.service';
-import { CreateMainCategoryDto } from './dto/create.catagory.dto';
-import { UpdateMainCatagoryDto } from './dto/update.catagory.dto';
-// import { UpdateCatagoryDto } from './dto/update.catagory.dto';
 
-@Controller('catagories')
+import { Body, Controller, Delete, Get, Param, Post, Put, Query } from '@nestjs/common';
+import { CatagoryService } from './catagory.service';
+import { CreateCategoryDto } from './dto/create.catagory.dto';
+import { UpdateCatagoryDto } from './dto/update.catagory.dto';
+
+
+
+@Controller('categories')
 export class CatagoryController {
     constructor( private readonly catagoryService:CatagoryService){}
     @Post('create')
-    async create(@Body() createMainCatagoryDto: CreateMainCategoryDto){
-     return this.catagoryService.create(createMainCatagoryDto)
+    async create(@Body() createCatagoryDto: CreateCategoryDto){
+     return this.catagoryService.create(createCatagoryDto)
     }
     @Get('/')
-    async findAll(){
+     findAll(){
         return this.catagoryService.findAll()
     }
+    @Get('root')
+    async getRootCategory() {
+      return this.catagoryService.getRootCategory();
+    }
+    @Get('with-products')
+    async getCategoriesWithProducts() {
+      
+      return this.catagoryService.getCategoriesWithProducts();
+    }
+
     @Get(':id')
     async findOne(@Param('id') id:string){
         return this.catagoryService.findOne(id)
+
     }
+   
     @Put(':id')
-    async Update(@Param('id') id:string ,@Body() updateMainCatagoryDto:UpdateMainCatagoryDto){
-        return this.catagoryService.update(id,updateMainCatagoryDto)
+    async Update(@Param('id') id:string ,@Body() updateCatagoryDto:UpdateCatagoryDto){
+        return this.catagoryService.update(id,updateCatagoryDto)
     }
     @Delete(':id')
     async remove(@Param('id') id:string){
         return this.catagoryService.remove(id)
 
     }
+  
 }
