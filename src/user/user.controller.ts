71cbab782e780ff63f57f34a148f654c2b7a7c36
bettytadege <bettytadege13@gmail.com>
2025-05-08 19:57:20 +0300

    refactor(user):add update route

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,17 +1,22 @@
 /* eslint-disable prettier/prettier */
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UpdateUserDto } from './dto/update.user.dto';
 
 @Controller('user')
 export class UserController {
     constructor(private readonly userService:UserService){}
+    @Get()
+       async findAll(){
+           return this.userService.findAll()
+       }
     @Get(':id')
        async findOne(@Param('id') id:string){
            return this.userService.findOne(id)
        }
-       @Post(':id')
-       async update(  @Param(':id') id:string, @Body() updateUserDto:UpdateUserDto){
+    
+       @Patch(':id')
+       async update(  @Param('id') id:string, @Body() updateUserDto:UpdateUserDto){
         return this.userService.update(id,updateUserDto)
        }
 }
