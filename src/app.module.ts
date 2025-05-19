cb4aa0c659b202b69bf237ecbab4666ecd8f939c
bettytadege <bettytadege13@gmail.com>
2025-05-19 10:30:14 +0300

    refactor:add order,cart,address,payment modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,6 +12,10 @@ import { ProductVariantModule } from './product-variant/product-variant.module';
 // import { SubCategoryModule } from './sub-category/sub-category.module';
 // import { SubSubCategoryModule } from './sub-sub-category/sub-sub-category.module';
 import { UserModule } from './user/user.module';
+import { OrderModule } from './order/order.module';
+import { CartModule } from './cart/cart.module';
+import { AddressModule } from './address/address.module';
+import { PaymentModule } from './payment/payment.module';
 
 
 
@@ -26,9 +30,12 @@ import { UserModule } from './user/user.module';
     CatagoryModule,
     ProductModule,
     ProductVariantModule,
-    // SubCategoryModule,
-    // SubSubCategoryModule,
+    OrderModule,
     UserModule,
+    OrderModule,
+    CartModule,
+    AddressModule,
+    PaymentModule,
   ],
 
   controllers: [AppController, ProductVariantController],
