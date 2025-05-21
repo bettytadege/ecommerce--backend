872cce8d2816a480e2bfe728998464b36d6a5154
bettytadege <bettytadege13@gmail.com>
2025-05-21 09:23:53 +0300

    refactor:add apioperation motes

--- src/catagory/catagory.controller.ts
+++ src/catagory/catagory.controller.ts
@@ -1,49 +1,66 @@
 /* eslint-disable prettier/prettier */
-/* eslint-disable @typescript-eslint/no-unsafe-return */
 
-
-import { Body, Controller, Delete, Get, Param, Post, Put, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
 import { CatagoryService } from './catagory.service';
 import { CreateCategoryDto } from './dto/create.catagory.dto';
 import { UpdateCatagoryDto } from './dto/update.catagory.dto';
-
-
+import { PaginationDto } from 'src/dto/pagnation.dto';
+import { ApiBody, ApiOperation, ApiQuery } from '@nestjs/swagger';
 
 @Controller('categories')
 export class CatagoryController {
-    constructor( private readonly catagoryService:CatagoryService){}
-    @Post('create')
-    async create(@Body() createCatagoryDto: CreateCategoryDto){
-     return this.catagoryService.create(createCatagoryDto)
-    }
-    @Get('/')
-     findAll(){
-        return this.catagoryService.findAll()
-    }
-    @Get('root')
-    async getRootCategory() {
-      return this.catagoryService.getRootCategory();
-    }
-    @Get('with-products')
-    async getCategoriesWithProducts() {
-      
-      return this.catagoryService.getCategoriesWithProducts();
-    }
-
-    @Get(':id')
-    async findOne(@Param('id') id:string){
-        return this.catagoryService.findOne(id)
-
-    }
-   
-    @Put(':id')
-    async Update(@Param('id') id:string ,@Body() updateCatagoryDto:UpdateCatagoryDto){
-        return this.catagoryService.update(id,updateCatagoryDto)
-    }
-    @Delete(':id')
-    async remove(@Param('id') id:string){
-        return this.catagoryService.remove(id)
-
-    }
+  constructor(private readonly catagoryService: CatagoryService) {}
+  @Post()
+  @ApiOperation({ summary: 'Create a new category' })
+  @ApiBody({ type: CreateCategoryDto })
+  async create(@Body() createCatagoryDto: CreateCategoryDto) {
+    return this.catagoryService.create(createCatagoryDto);
+  }
+  @Get('/')
+  // @ApiQuery({
+ 
+  @ApiOperation({ summary: 'get all categories' })
+  findAll() {
+    return this.catagoryService.findAll();
+  }
   
+  @Get('root')
+  @ApiOperation({ summary: 'get root categories' })
+  async getRootCategory() {
+    return this.catagoryService.getRootCategory();
+  }
+
+  @Get('with-products')
+  async getCategoriesWithProducts() {
+    return this.catagoryService.getCategoriesWithProducts();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string) {
+    return this.catagoryService.findOne(id);
+  }
+
+  @Put(':id')
+  @ApiOperation({ summary: 'update category' })
+  async Update(
+    @Param('id') id: string,
+    @Body() updateCatagoryDto: UpdateCatagoryDto,
+  ) {
+    return this.catagoryService.update(id, updateCatagoryDto);
+  }
+
+  @Delete(':id')
+  @ApiOperation({ summary: 'remove category' })
+  async remove(@Param('id') id: string) {
+    return this.catagoryService.remove(id);
+  }
 }
