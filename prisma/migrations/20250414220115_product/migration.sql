ecad3f48a5722f5d3469f04acb2f161733a5d6dc
bettytadege <bettytadege13@gmail.com>
2025-06-13 10:44:23 +0300

    refactor:remove migration

--- prisma/migrations/20250414220115_product/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `attribute` on the `SubSubCategory` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "SubSubCategory" DROP COLUMN "attribute";
