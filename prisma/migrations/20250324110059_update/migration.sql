98ff4ccbf7f568405a4fed28f04ef94b10f71630
bettytadege <bettytadege13@gmail.com>
2025-06-13 10:42:26 +0300

    refactor:remove migration

--- prisma/migrations/20250324110059_update/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE "Catagory" DROP CONSTRAINT "Catagory_parentId_fkey";
-
--- AddForeignKey
-ALTER TABLE "Catagory" ADD CONSTRAINT "Catagory_parentId_fkey" FOREIGN KEY ("parentId") REFERENCES "Catagory"("id") ON DELETE CASCADE ON UPDATE CASCADE;
