38ebb747e2fd28c9c20c70e68ebdc9d8df0f0413
bettytadege <bettytadege13@gmail.com>
2025-06-17 11:28:20 +0300

    refcator:returning json message

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,7 +3,10 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello() {
+     return {
+    message: 'please check the documentations',
+    data: '/api',
+  };
   }
 }
